64d7a97e2a3cd8c2a4307283b4cdf0ee5ac4110a
Justin Rockwood <justinrockwood@hotmail.com>
2020-09-18 12:36:14 -0700

    Add test project

---
+++ Source/SharpMediaFoundation.SampleApp/MainForm.cs
@@ -0,0 +1,19 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainForm.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace SharpMediaFoundation.SampleApp
+{
+    using System.Windows.Forms;
+
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Source/SharpMediaFoundation.SampleApp/Program.cs
@@ -0,0 +1,31 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace SharpMediaFoundation.SampleApp
+{
+    using System;
+    using System.Windows.Forms;
+
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            MediaFoundation.Startup();
+
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+
+            MediaFoundation.Shutdown();
+        }
+    }
+}
